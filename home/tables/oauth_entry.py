cc8ede92c4c5d9cd8b024c9e57831a5804ab1506
skelmis <skelmis.craft@gmail.com>
2025-10-26 16:38:22 +1300

    Add optional access and refresh token storage

--- home/tables/oauth_entry.py
+++ home/tables/oauth_entry.py
@@ -12,6 +12,8 @@ from piccolo.columns.column_types import Serial, ForeignKey
 from piccolo.columns.indexes import IndexMethod
 from piccolo.table import Table
 
+from home import constants
+
 
 class OAuthEntry(Table, tablename="oauth_entry"):
     if TYPE_CHECKING:
@@ -23,6 +25,18 @@ class OAuthEntry(Table, tablename="oauth_entry"):
         index_method=IndexMethod.hash,
     )
     oauth_email = Text(help_text="The email address of this person in OAuth land")
+    access_token_raw = Text(
+        help_text="The access token for this person in OAuth land",
+        null=True,
+        default=None,
+        secret=True,
+    )
+    refresh_token_raw = Text(
+        help_text="The refresh token for this person in OAuth land",
+        null=True,
+        default=None,
+        secret=True,
+    )
     provider = Text(help_text="The OAuth provider")
     user = ForeignKey(LazyTableReference("Users", module_path="home.tables"))
     last_login = Timestamptz(
@@ -31,3 +45,19 @@ class OAuthEntry(Table, tablename="oauth_entry"):
         required=False,
         help_text="When this user last logged in using this provider.",
     )
+
+    @property
+    def access_token(self) -> str:
+        return constants.ENCRYPTION_PROVIDER.decrypt(self.access_token_raw)
+
+    @access_token.setter
+    def access_token(self, value) -> None:
+        self.access_token_raw = constants.ENCRYPTION_PROVIDER.encrypt(value)
+
+    @property
+    def refresh_token(self) -> str:
+        return constants.ENCRYPTION_PROVIDER.decrypt(self.refresh_token_raw)
+
+    @refresh_token.setter
+    def refresh_token(self, value) -> None:
+        self.refresh_token_raw = constants.ENCRYPTION_PROVIDER.encrypt(value)
