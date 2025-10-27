3d7286f0c27399d2c4ef1d5baea6701e306d5d14
skelmis <skelmis.craft@gmail.com>
2025-10-27 13:34:08 +1300

    squash migrations

--- home/piccolo_migrations/home_2025_10_27t13_31_08_066636.py
+++ home/piccolo_migrations/home_2025_10_27t13_31_08_066636.py
@@ -3,9 +3,10 @@ from enum import Enum
 from home.util.table_mixins import utc_now
 from piccolo.columns.base import OnDelete
 from piccolo.columns.base import OnUpdate
-from piccolo.columns.column_types import Boolean, Serial
+from piccolo.columns.column_types import Boolean
 from piccolo.columns.column_types import ForeignKey
 from piccolo.columns.column_types import Secret
+from piccolo.columns.column_types import Serial
 from piccolo.columns.column_types import Text
 from piccolo.columns.column_types import Timestamptz
 from piccolo.columns.column_types import UUID
@@ -29,7 +30,7 @@ class Users(Table, tablename="users", schema=None):
     )
 
 
-ID = "2025-10-25T17:42:39:127824"
+ID = "2025-10-27T13:31:08:066636"
 VERSION = "1.30.0"
 DESCRIPTION = ""
 
@@ -37,6 +38,24 @@ DESCRIPTION = ""
 async def forwards():
     manager = MigrationManager(
         migration_id=ID, app_name="home", description=DESCRIPTION
+    )
+
+    manager.add_table(
+        class_name="OAuthEntry",
+        tablename="oauth_entry",
+        schema=None,
+        columns=None,
+    )
+
+    manager.add_table(
+        class_name="AuthenticationAttempts",
+        tablename="authentication_attempts",
+        schema=None,
+        columns=None,
+    )
+
+    manager.add_table(
+        class_name="Alerts", tablename="alerts", schema=None, columns=None
     )
 
     manager.add_table(
@@ -50,15 +69,357 @@ async def forwards():
         columns=None,
     )
 
-    manager.add_table(
-        class_name="OAuthEntry",
+    manager.add_column(
+        table_class_name="OAuthEntry",
         tablename="oauth_entry",
+        column_name="oauth_id",
+        db_column_name="oauth_id",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": True,
+            "index_method": IndexMethod.hash,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
         schema=None,
-        columns=None,
     )
 
-    manager.add_table(
-        class_name="Alerts", tablename="alerts", schema=None, columns=None
+    manager.add_column(
+        table_class_name="OAuthEntry",
+        tablename="oauth_entry",
+        column_name="oauth_email",
+        db_column_name="oauth_email",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="OAuthEntry",
+        tablename="oauth_entry",
+        column_name="access_token_raw",
+        db_column_name="access_token_raw",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": None,
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": True,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="OAuthEntry",
+        tablename="oauth_entry",
+        column_name="refresh_token_raw",
+        db_column_name="refresh_token_raw",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": None,
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": True,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="OAuthEntry",
+        tablename="oauth_entry",
+        column_name="provider",
+        db_column_name="provider",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="OAuthEntry",
+        tablename="oauth_entry",
+        column_name="user",
+        db_column_name="user",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Users,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="OAuthEntry",
+        tablename="oauth_entry",
+        column_name="last_login",
+        db_column_name="last_login",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": None,
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="AuthenticationAttempts",
+        tablename="authentication_attempts",
+        column_name="user",
+        db_column_name="user",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Users,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": True,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="AuthenticationAttempts",
+        tablename="authentication_attempts",
+        column_name="attempt_made_at",
+        db_column_name="attempt_made_at",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="AuthenticationAttempts",
+        tablename="authentication_attempts",
+        column_name="attempt_made_via",
+        db_column_name="attempt_made_via",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Alerts",
+        tablename="alerts",
+        column_name="created_at",
+        db_column_name="created_at",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": utc_now,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Alerts",
+        tablename="alerts",
+        column_name="last_modified_at",
+        db_column_name="last_modified_at",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Alerts",
+        tablename="alerts",
+        column_name="uuid",
+        db_column_name="uuid",
+        column_class_name="UUID",
+        column_class=UUID,
+        params={
+            "default": UUID4(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": True,
+            "index_method": IndexMethod.hash,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Alerts",
+        tablename="alerts",
+        column_name="target",
+        db_column_name="target",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Users,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": True,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Alerts",
+        tablename="alerts",
+        column_name="message",
+        db_column_name="message",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Alerts",
+        tablename="alerts",
+        column_name="level",
+        db_column_name="level",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": Enum(
+                "AlertLevels",
+                {
+                    "INFO": "info",
+                    "WARNING": "warning",
+                    "ERROR": "error",
+                    "SUCCESS": "success",
+                },
+            ),
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
     )
 
     manager.add_column(
@@ -416,251 +777,6 @@ async def forwards():
             "index": False,
             "index_method": IndexMethod.btree,
             "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="OAuthEntry",
-        tablename="oauth_entry",
-        column_name="oauth_id",
-        db_column_name="oauth_id",
-        column_class_name="Text",
-        column_class=Text,
-        params={
-            "default": "",
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": True,
-            "index_method": IndexMethod.hash,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="OAuthEntry",
-        tablename="oauth_entry",
-        column_name="oauth_email",
-        db_column_name="oauth_email",
-        column_class_name="Text",
-        column_class=Text,
-        params={
-            "default": "",
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="OAuthEntry",
-        tablename="oauth_entry",
-        column_name="provider",
-        db_column_name="provider",
-        column_class_name="Text",
-        column_class=Text,
-        params={
-            "default": "",
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="OAuthEntry",
-        tablename="oauth_entry",
-        column_name="user",
-        db_column_name="user",
-        column_class_name="ForeignKey",
-        column_class=ForeignKey,
-        params={
-            "references": Users,
-            "on_delete": OnDelete.cascade,
-            "on_update": OnUpdate.cascade,
-            "target_column": None,
-            "null": True,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="OAuthEntry",
-        tablename="oauth_entry",
-        column_name="last_login",
-        db_column_name="last_login",
-        column_class_name="Timestamptz",
-        column_class=Timestamptz,
-        params={
-            "default": None,
-            "null": True,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="Alerts",
-        tablename="alerts",
-        column_name="created_at",
-        db_column_name="created_at",
-        column_class_name="Timestamptz",
-        column_class=Timestamptz,
-        params={
-            "default": utc_now,
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="Alerts",
-        tablename="alerts",
-        column_name="last_modified_at",
-        db_column_name="last_modified_at",
-        column_class_name="Timestamptz",
-        column_class=Timestamptz,
-        params={
-            "default": TimestamptzNow(),
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="Alerts",
-        tablename="alerts",
-        column_name="uuid",
-        db_column_name="uuid",
-        column_class_name="UUID",
-        column_class=UUID,
-        params={
-            "default": UUID4(),
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": True,
-            "index_method": IndexMethod.hash,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="Alerts",
-        tablename="alerts",
-        column_name="target",
-        db_column_name="target",
-        column_class_name="ForeignKey",
-        column_class=ForeignKey,
-        params={
-            "references": Users,
-            "on_delete": OnDelete.cascade,
-            "on_update": OnUpdate.cascade,
-            "target_column": None,
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": True,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="Alerts",
-        tablename="alerts",
-        column_name="message",
-        db_column_name="message",
-        column_class_name="Text",
-        column_class=Text,
-        params={
-            "default": "",
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": None,
-            "db_column_name": None,
-            "secret": False,
-        },
-        schema=None,
-    )
-
-    manager.add_column(
-        table_class_name="Alerts",
-        tablename="alerts",
-        column_name="level",
-        db_column_name="level",
-        column_class_name="Text",
-        column_class=Text,
-        params={
-            "default": "",
-            "null": False,
-            "primary_key": False,
-            "unique": False,
-            "index": False,
-            "index_method": IndexMethod.btree,
-            "choices": Enum(
-                "AlertLevels",
-                {
-                    "INFO": "info",
-                    "WARNING": "warning",
-                    "ERROR": "error",
-                    "SUCCESS": "success",
-                },
-            ),
             "db_column_name": None,
             "secret": False,
         },
